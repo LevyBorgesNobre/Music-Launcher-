4daa9c57b252ec714f58f7d0bb0f63a2aa8bc6a9
Levy Borges <levyborges007@gmail.com>
2025-05-21 20:04:04 -0300

    refactor:  updating the api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api  = axios.create({
-    baseURL:'https://mlauncherapi-production.up.railway.app/'
+    baseURL:'https://musiclauncherapi.onrender.com/'
 })
 
 api.interceptors.request.use((config)=> {
